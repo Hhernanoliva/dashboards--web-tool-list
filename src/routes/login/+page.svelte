843e639c231c276a5c8229da9ec7860361788316
malks14 <greco.guido@hotmail..com>
2023-11-06 09:47:14 -0300

    initial store

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,7 +1,10 @@
 <!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
 <script lang="ts">
+    import PocketBase from 'pocketbase';
     import {goto} from "$app/navigation"
-
+    import {userStore} from "$lib/store"
+    console.log(userStore);
+    
 	let identity: String;
 	let password: String;
 	let roleOption = '';
@@ -11,6 +14,7 @@
 		{ value: 'public', label: 'Public' }
 	];
 
+    const pb = new PocketBase('https://damp-tiger.pockethost.io');
 	const handleSubmit = async (event: Event) => {
 		event.preventDefault();
 		if (roleOption === 'admin') {
@@ -32,10 +36,13 @@
 				if (!response.ok) {
 					throw new Error(`HTTP error! status: ${response.status}`);
 				}
-
-				const data = await response.json();
-				console.log(data);
-                await goto('/');
+                const authData = await pb.admins.authWithPassword(identity, password);
+                console.log(authData);
+                console.log(pb.authStore);
+			// 	const data = await response.json();
+            //    let test = userStore.update(currentValue => ({ ...currentValue, authData }));
+			// 	console.log(test);
+                // await goto('/');
 			} catch (error) {
 				console.log(error);
 			}
@@ -68,12 +75,14 @@
 		}
 	};
 </script>
-
-<form on:submit={handleSubmit} class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-5">
-		<label for="identity" class="label">
-			<span>Email</span>
-			<input
+<div class="container">
+    <h1>Login Page</h1>
+    <form on:submit={handleSubmit} class="container h-full mx-auto flex justify-center items-center">
+        
+        <div class="space-y-5">
+            <label for="identity" class="label">
+                <span>Email</span>
+                <input
 				class="input"
 				name="identity"
 				id="identity"
@@ -81,12 +90,12 @@
 				placeholder="Colocar Email"
 				bind:value={identity}
 				required
-			/>
-		</label>
-
-		<label for="password" class="label">
-			<span>Contraseña</span>
-			<input
+                />
+            </label>
+            
+            <label for="password" class="label">
+                <span>Contraseña</span>
+                <input
 				class="input"
 				name="password"
 				id="password"
@@ -94,16 +103,18 @@
 				placeholder="Insertar contraseña"
 				bind:value={password}
 				required
-			/>
-		</label>
-		<label for="role" class="label">
-			<span>Role</span>
-			<select bind:value={roleOption} class="select">
-				{#each options as option (option.value)}
+                />
+            </label>
+            <label for="role" class="label">
+                <span>Role</span>
+                <select bind:value={roleOption} class="select">
+                    {#each options as option (option.value)}
 					<option value={option.value}>{option.label}</option>
-				{/each}
-			</select>
-		</label>
-		<button type="submit" class="btn variant-filled">Submit</button>
-	</div>
-</form>
+                    {/each}
+                </select>
+            </label>
+            <button type="submit" class="btn variant-filled">Submit</button>
+        </div>
+    </form>
+</div>
+    
