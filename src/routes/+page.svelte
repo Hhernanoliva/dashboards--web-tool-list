138369b3e0e0482b8851ece49ba6107738adc30a
Hernan Oliva <hhernanoliva@gmail.com>
2023-11-06 17:55:48 -0300

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,14 @@
 
 <script>
 import SearchBar from "../lib/components/SearchBar/SearchBar.svelte"
+import Card from "../lib/components/Card/card.svelte";
 </script>
 
-<div class="container h-full mx-auto flex justify-center items-center">
+<div class="container flex items-center justify-center h-full mx-auto">
 	<div class="space-y-5">
 		<SearchBar />
-		<h1 class="h1">Let's get cracking bones!</h1>
+		<h1 class="h1">Links</h1>
+		<Card title="SvelteKit"/>
 		<p>Start by exploring:</p>
 		<ul>
 			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout</li>
